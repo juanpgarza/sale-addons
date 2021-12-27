4964c72fe31dfbf159c5885f528c70a33de4b212
Juan Pablo Garza <juanp@juanpgarza.com>
2021-12-27 09:32:22 -0300

    [12.0][IMP] t656 sale_quotation_template_ux: emprolijar archivos y borrar código comentado

--- sale_quotation_template_ux/models/sale_order.py
+++ sale_quotation_template_ux/models/sale_order.py
@@ -7,26 +7,6 @@ class SaleOrder(models.Model):
 
     website_description_2 = fields.Html('Website Description 2', sanitize_attributes=False, translate=html_translate)
 
-    # @api.onchange('partner_id')
-    # def onchange_update_description_lang(self):
-    #     super(SaleOrder,self).onchange_update_description_lang()
-    #     if not self.sale_order_template_id:
-    #         return
-    #     else:
-    #         template = self.sale_order_template_id.with_context(lang=self.partner_id.lang)
-    #         import pdb; pdb.set_trace()
-    #         self.website_description_2 = template.website_description_2
-
-    # def _compute_line_data_for_template_change(self, line):
-    #     vals = super(SaleOrder, self)._compute_line_data_for_template_change(line)
-    #     vals.update(website_description_2=line.website_description_2)
-    #     return vals
-
-    # def _compute_option_data_for_template_change(self, option):
-    #     vals = super(SaleOrder, self)._compute_option_data_for_template_change(option)
-    #     vals.update(website_description_2=option.website_description_2)
-    #     return vals
-
     @api.onchange('sale_order_template_id')
     def onchange_sale_order_template_id(self):
         ret = super(SaleOrder, self).onchange_sale_order_template_id()
