75e10cc7f7e7ff7e92c03d8d9c417ea7937e8d06
Juan Pablo Garza <juanp@juanpgarza.com>
2024-11-25 15:03:04 +0000

    [MIG] sale_commission_order: Migration to 17.0

--- sale_commission_order/__manifest__.py
+++ sale_commission_order/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "sale_commission_order",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "ITSur",
     "website": "",
 
