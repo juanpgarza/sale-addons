6feec6f9320783bc1a8a7ea0a1f78c539e85c59a
Juan Pablo Garza <jgarza@juanpgarza.com>
2025-06-15 14:22:23 +0000

    [MIG] sale_order_quote_ux: Migration to 18.0 --> WIP!

--- sale_order_quote_ux/__manifest__.py
+++ sale_order_quote_ux/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "sale_order_quote_ux",
     "summary": "",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/juanpgarza/sale-addons",
     "author": "juanpgarza",
