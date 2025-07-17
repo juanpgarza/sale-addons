fde8a5ce16e149da61925fc9156c0f8e7472a60a
Juan Pablo Garza <juanp@juanpgarza.com>
2025-07-17 07:56:10 -0300

    [MIG] sale_order_margin_percent_ux: Migration to 18.0

--- sale_order_margin_percent_ux/__manifest__.py
+++ sale_order_margin_percent_ux/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "sale_order_margin_percent_ux",
     "summary": "",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/juanpgarza/sale-addons",
     'license': 'AGPL-3',
@@ -15,5 +15,5 @@
         ],
     "data": [
         ],
-    "installable": False,
+    "installable": True,
 }
